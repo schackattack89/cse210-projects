c05595e18afab9572399750ddce14d8de2e3c96b
schackattack89 <nschack@gmail.com>
2024-01-17 13:48:23 -0500

    added external prompts file (.txt) to pull from

--- prove/Develop02/PromptGenerator.cs
+++ prove/Develop02/PromptGenerator.cs
@@ -3,14 +3,20 @@ public class PromptGenerator{
     public string GetRandomPrompt()
     {
         _prompts = new List<string>();
-        _prompts.Add("What was the best part of my day?");
-        _prompts.Add("What could I have accomplished today with 1 extra hour?");
-        _prompts.Add("Who had an impact on my life today?");
-        _prompts.Add("What made me smile today?");
-        _prompts.Add("If I could do over 1 thing from today what what is?");
+        string filename = "Journaling_Prompts.txt";
+        string [] lines = System.IO.File.ReadAllLines(filename);
+        foreach(string line in lines){
+            _prompts.Add(line);
+        }
+        
+        //_prompts.Add("What was the best part of my day?");
+        //_prompts.Add("What could I have accomplished today with 1 extra hour?");
+        //_prompts.Add("Who had an impact on my life today?");
+        //_prompts.Add("What made me smile today?");
+        //_prompts.Add("If I could do over 1 thing from today what what is?");
         
         Random randomGenerator = new Random();
-        int promptChoice = randomGenerator.Next(0,4);
+        int promptChoice = randomGenerator.Next(0,30);
 
         return _prompts[promptChoice];
     }
